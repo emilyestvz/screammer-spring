4be2a29eb3981cbc8be00c213700aa8a915bab5c
Emily <emilys@genstudents.org>
2025-07-24 13:30:34 -0300

    perf: Searching for complete Series data

--- src/main/java/br/com/alura/scrammer_spring/ScrammerSpringApplication.java
+++ src/main/java/br/com/alura/scrammer_spring/ScrammerSpringApplication.java
@@ -1,16 +1,9 @@
 package br.com.alura.scrammer_spring;
 
-import br.com.alura.scrammer_spring.model.DadosSerie;
-import br.com.alura.scrammer_spring.model.DadosEpisodio;
-import br.com.alura.scrammer_spring.model.DadosTemporada;
-import br.com.alura.scrammer_spring.service.ConsumoApi;
-import br.com.alura.scrammer_spring.service.ConverteDados;
+import br.com.alura.scrammer_spring.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScrammerSpringApplication implements CommandLineRunner {
@@ -19,36 +12,9 @@ public class ScrammerSpringApplication implements CommandLineRunner {
 		SpringApplication.run(ScrammerSpringApplication.class, args);
 	}
 
-	// Consumindo API e convertendo JSON para objeto Java
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-
-		// Consumindo API e convertendo JSON para objeto Java em DadosSerie
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		// Consumindo API atualizada e convertendo JSON para objeto Java em Episodios
-		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		// Exibindo as temporadas e seus episódios
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i <= dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
-			var dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-
-		}
-
-		temporadas.forEach(System.out::println);
-
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
--- src/main/java/br/com/alura/scrammer_spring/principal/Principal.java
+++ src/main/java/br/com/alura/scrammer_spring/principal/Principal.java
@@ -1,7 +1,12 @@
 package br.com.alura.scrammer_spring.principal;
 
+import br.com.alura.scrammer_spring.model.DadosSerie;
+import br.com.alura.scrammer_spring.model.DadosTemporada;
 import br.com.alura.scrammer_spring.service.ConsumoApi;
+import br.com.alura.scrammer_spring.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -9,70 +14,37 @@ public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumo = new ConsumoApi();
 
+    private ConverteDados conversor = new ConverteDados();
+
     // URL base da API OMDB
     private final String ENDERECO ="https://omdbapi.com/?t=";
     private final String API_KEY ="&apikey=6585022c";
 
     public void exibeMenu() {
         Scanner scanner = new Scanner(System.in);
-        int escolha = 0;
+        System.out.println("\n=========================================");
+        System.out.println("\t\tBem-vindo ao Scrammer! ✨");
+        System.out.println("=========================================");
+        System.out.println("Digite o nome da série que deseja pesquisar: ");
+
         var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
-        var json = consumo.obterDados(ENDERECO + nomeSerie + API_KEY);
+        System.out.println(dados);
 
-        while (true) {
-            System.out.println("Bem-vindo(a) ao Screammer Spring! ✨");
-            System.out.println("\nSelecione uma opção:");
-            System.out.println("1. Buscar série por nome");
-            System.out.println("2. Buscar episódio por nome");
-            System.out.println("3. Buscar temporada por número");
-            System.out.println("4. Sair");
-            System.out.println("\nDigite sua escolha: ");
+        // Exibindo as temporadas e seus episódios
+		List<DadosTemporada> temporadas = new ArrayList<>();
 
-            if (scanner.hasNextInt()) {
-                escolha = scanner.nextInt();
-                scanner.nextLine(); // Limpa o buffer
-            } else {
-                System.out.println("Entrada inválida. Por favor, digite um número.");
-                scanner.nextLine(); // Limpa entrada inválida
-                continue;
-            }
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+			var dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
 
-            switch (escolha) {
-                case 1:
-                    System.out.println("\nDigite o nome da série que deseja buscar: ");
-                    String urlSerie = ENDERECO + nomeSerie.replace(" ", "+") + API_KEY;
-                    System.out.println("\nVocê buscou pela série: " + nomeSerie);
-                    System.out.println("URL de busca: " + urlSerie);
-                    break;
-                case 2:
-                    System.out.println("\nDigite o nome do episódio que deseja buscar: ");
-                    String nomeEpisodio = scanner.nextLine();
-                    String urlEpisodio = ENDERECO + nomeEpisodio.replace(" ", "+") + API_KEY;
-                    System.out.println("\nVocê buscou pelo episódio: " + nomeEpisodio);
-                    System.out.println("URL de busca: " + urlEpisodio);
-                    break;
-                case 3:
-                    System.out.println("\nDigite o número da temporada que deseja buscar: ");
-                    if (scanner.hasNextInt()) {
-                        int numeroTemporada = scanner.nextInt();
-                        scanner.nextLine(); // Limpa o buffer
-                        String urlTemporada = ENDERECO + "season+" + numeroTemporada + API_KEY;
-                        System.out.println("\nVocê buscou pela temporada número: " + numeroTemporada);
-                        System.out.println("URL de busca: " + urlTemporada);
-                    } else {
-                        System.out.println("Número inválido.");
-                        scanner.nextLine(); // Limpa entrada inválida
-                    }
-                    break;
-                case 4:
-                    System.out.println("Saindo do programa. Até logo! ✨");
-                    scanner.close();
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Opção inválida. Por favor, tente novamente.");
-            }
-        }
+		temporadas.forEach(System.out::println);
+
+
     }
 }
+
